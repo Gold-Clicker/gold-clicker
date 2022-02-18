47786d1b7ca4e1316795c49f41695e841c4f6cb3
sc-galaxyM31 <99510419+scM31@users.noreply.github.com>
2022-02-18 17:40:57 -0500

    Update achieve.js

--- code/achieve.js
+++ code/achieve.js
@@ -1,9 +1,3 @@
-let i = 0;
-function showAchieve() {
-  if(i <= 0) {
-    document.getElementyId("bigCase").innerHTML = " ";
-    document.getElementyId("archive").innerHTML = archive;
-  } else {
-    
-  }
-}
+//achieve.js will also have hacks, tabs, etc.
+
+//hacks
