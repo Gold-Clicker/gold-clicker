4c9e809a18023125c7fb0a6208de308e46301299
sc-galaxyM31 <99510419+scM31@users.noreply.github.com>
2022-02-25 07:41:53 -0500

    Update store.js

--- code/store.js
+++ code/store.js
@@ -1,14 +1,3 @@
-//Price Tags
-setInterval(Price, 100);
-function Price() {
-  document.getElementById("lp").innerHTML = "G" + Math.round(lemPrice);
-  document.getElementById("bp").innerHTML = "G" + Math.round(browPrice);
-  document.getElementById("walp").innerHTML = "G" + Math.round(wallPrice);
-  document.getElementById("worp").innerHTML = "G" + Math.round(workPrice);
-  document.getElementById("gp").innerHTML = "G" + Math.round(goldPrice);
-  document.getElementById("celp").innerHTML = "G" + Math.round(cellPrice);
-  document.getElementById("cryp").innerHTML = "G" + Math.round(cryPrice);
-}
 //Lemonades
 let lem = 0;
 let lemPrice = 10;
@@ -44,7 +33,7 @@ function searchLem() {
 }
 //Brownies
 let brow = 0;
-let browPrice = 100;
+let browPrice = 50;
 function brownie() {
   if (val >= browPrice) {
     brow += 1;
@@ -77,7 +66,7 @@ function searchBrow() {
 }
 //Wallets
 let wall = 0;
-let wallPrice = 1000;
+let wallPrice = 100;
 function wallet() {
   if(val >= wallPrice) {
     val -= wallPrice;
@@ -113,7 +102,7 @@ function searchWall() {
 }
 //Workers
 let work = 0;
-let workPrice = 10000;
+let workPrice = 500;
 function worker() {
   if(val >= workPrice) {
     work += 1;
@@ -146,7 +135,7 @@ function searchWork() {
 }
 //Gold Bars
 let gold = 0;
-let goldPrice = 100000;
+let goldPrice = 1000;
 function goldBar() {
   if(val >= goldPrice) {
     add += Math.round(Math.random() * 11) + 1;
@@ -177,35 +166,9 @@ function searchGold() {
     document.getElementById("igold").style.display = "block";
   }
 }
-//Cell Upgrade, Hacks into CSS property | ONLY PURCHASABLE ONCE |
-let cell = 0;
-let cellPrice = 1000000;
-function cellCursor() {
-  if(val >= cellPrice) {
-    document.getElementById("body").style.cursor = "cell";
-    document.getElementById("Dollar").style.cursor = "cell";
-    document.getElementById("cell").style.display = "none";
-    val -= cellPrice;
-    cell += 1;
-    add += 100;
-  }
-}
-setInterval(cellRun, 100);
-function cellRun() {
-  if(val >= cellPrice && cell < 1) {
-    document.getElementById("cell").style.display = "block";
-  } else {
-    document.getElementById("cell").style.display = "none";
-  }
-  if(cell >= 1) {
-    document.getElementById("icellc").style.display = "block";
-  } else {
-    document.getElementById("icellc").style.display = "none";
-  }
-}
 //Crypto
 let cry = 0;
-let cryPrice = 10000000;
+let cryPrice = 10000;
 function crypto() {
   if(val >= cryPrice) {
     add += add / 10;
@@ -220,7 +183,7 @@ function crypto() {
     document.getElementById("cryPrice").innerHTML = "&nbsp;Cost in Gold: " + cryPrice;
   }
   function cryRun() {
-    val += val / 10;
+    val += val / 100;
   }
 }
 setInterval(searchCry, 100);
@@ -235,4 +198,73 @@ function searchCry() {
   } else {
     document.getElementById("icrypto").style.display = "block";
   }
+}
+//bubble gum
+let gum = 0;
+let gumPrice = 10000;
+function bubblegum() {
+  if(val >= gumPrice) {
+    const sure = window.confirm("Are you sure you want too buy this Item?");
+    if(sure) {
+    gumPrice += gumPrice * gumPrice;
+    val -= val * val;
+    gum += 1;
+    document.getElementById("start").innerHTML = "Negative Money, Is that even possible??!?!?!?!?!?!"
+    document.getElementById("bubblegum").style.display = "none"
+    } else {
+      document.getElementById("start").innerHTML = "Safely avoided unmeasurable apocalypse."
+  }
+  }
+}
+setInterval(searchGum, 100);
+function searchGum() {
+  if(val < gumPrice) {
+    document.getElementById("bubblegum").style.display = "none";
+  }
+}
+//Thief
+/*
+let thief = 0;
+let thiefPrice = 50000;
+function thief() {
+   if(val >= thiefPrice) {
+      val -= thiefPrice;
+      thief += 1;
+      thiefPrice += thiefPrice / 10;
+      setInterval(thiefRun, 1000);
+      setInterval(price, 100);
+      document.getElementById("thi").innerHTML = "Thieves: " + thief;
+   }
+   function thiefRun() {
+      va -= va           l
+   }
+   function price() {
+      document.getElementById("thiefPrice").innerHTML = "Cost in Gold: " + thiefPrice;
+   }
+}
+setInterval(searchThi, 100);
+function searchThi() {
+  if(val < thiefPrice) {
+    document.getElementById("thief").style.display = "none";
+  } else {
+    document.getElementById("thief").style.display = "block";
+  }
+  if(thief < 1) {
+    document.getElementById("ithief").style.display = "none";
+  } else {
+    document.getElementById("ithief").style.display = "block";
+  }
+}
+*/
+//Price Tags
+setInterval(Price, 100);
+function Price() {
+  document.getElementById("lp").innerHTML = "G" + Math.round(lemPrice);
+  document.getElementById("bp").innerHTML = "G" + Math.round(browPrice);
+  document.getElementById("walp").innerHTML = "G" + Math.round(wallPrice);
+  document.getElementById("worp").innerHTML = "G" + Math.round(workPrice);
+  document.getElementById("gp").innerHTML = "G" + Math.round(goldPrice);
+  document.getElementById("celp").innerHTML = "G" + Math.round(cellPrice);
+  document.getElementById("cryp").innerHTML = "G" + Math.round(cryPrice);
+  document.getElementById("thip").innerHTML = "G" + Math.round(thiefPrice); 
 }
