95a5e54393b994dbd734ac297965d1ee7977fe16
sc-galaxyM31 <99510419+scM31@users.noreply.github.com>
2022-04-09 14:34:55 -0400

    Add files via upload

---
+++ code/JS/codes.js
@@ -0,0 +1,18 @@
+alert("je");
+const codes = [
+  "start"
+];
+
+function activateCode(text) {
+  if(text === "start") {
+    val += 100000000000;
+  }
+}
+
+
+function codes() {
+  const input = document.getElementById("inputCode");
+  const text = input.value;
+  
+  activateCode(text);
+}
